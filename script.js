21e2cc5915ae614c78eb3d458e998d7e864dadb7
Rafal Suski <rsuski7@wp.pl>
2020-10-08 12:32:25 +0200

    add function klikniecieKaty script.js

--- script.js
+++ script.js
@@ -5,31 +5,50 @@ const kartyDoGry = ["Ahri", "Ahri", "Ashe", "Ashe", "Diana", "Diana",
 let karty = document.querySelectorAll("div");
 karty = [...karty];
 
-let czasPoczatekGry = new Date();
-let iloscZebranychPar = 0;
-let liczbaPar = karty.lenght;
-let liczbaKlikniecPar = 0;
+const czasPoczatekGry = new Date().getTime();
+
 let aktywnaKarta = "";
 const aktywne2Karty = [];
+const liczbaPar = karty.length/2;
+let iloscZebranychPar = 0;
+
+let liczbaKlikniecPar = 0;
 
 const klikniecieKarty = function() {
     aktywnaKarta = this;
-    aktywnaKarta.classList.remove("hidden")
-    if (aktywne2Karty.length ===0){
+    aktywnaKarta.classList.remove("hidden");
+
+    if (aktywne2Karty.length === 0){
         aktywne2Karty[0] = aktywnaKarta;
         return;
     }
     else {
         karty.forEach(karta =>{
-            karta.removeEventListener('click', klikniecieKarty);
-        })
+            karta.removeEventListener('click', klikniecieKarty) });
         aktywne2Karty[1] = aktywnaKarta;
+        liczbaKlikniecPar++;
+
         setTimeout(function(){
             if (aktywne2Karty[0].className === aktywne2Karty[1].className){
-                aktywne2Karty.forEach( karta => {
-                    karta.classList.add ("off");
-                })
+                aktywne2Karty.forEach( karta => karta.classList.add ("off"));
+            
+                iloscZebranychPar++;
+            
+                if (iloscZebranychPar == liczbaPar){
+                    const czasKoniecGry = new Date().getTime();
+                    const czasGry = (czasKoniecGry - czasPoczatekGry)/1000
+                
+                    alert(`WYGRALES!!! Twój czas to: ${czasGry} . Ilość prób to ${liczbaKlikniecPar}`);
+                    location.reload();
+                }
             }
+            else{
+                aktywne2Karty.forEach (karta => karta.classList.add("hidden"));
+            }
+            aktywnaKarta = "";
+            aktywne2Karty.length = 0;
+            
+            karty.forEach( karta => karta.addEventListener('click', klikniecieKarty));
         },500)
     }
 }
