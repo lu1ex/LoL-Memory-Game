31700d5cbe38e937e098d2590b1f868ec7d664cc
Rafal Suski <rsuski7@wp.pl>
2020-10-08 11:22:23 +0200

    add opcja trafienia pary script.js

--- script.js
+++ script.js
@@ -9,6 +9,30 @@ let czasPoczatekGry = new Date();
 let iloscZebranychPar = 0;
 let liczbaPar = karty.lenght;
 let liczbaKlikniecPar = 0;
+let aktywnaKarta = "";
+const aktywne2Karty = [];
+
+const klikniecieKarty = function() {
+    aktywnaKarta = this;
+    aktywnaKarta.classList.remove("hidden")
+    if (aktywne2Karty.length ===0){
+        aktywne2Karty[0] = aktywnaKarta;
+        return;
+    }
+    else {
+        karty.forEach(karta =>{
+            karta.removeEventListener('click', klikniecieKarty);
+        })
+        aktywne2Karty[1] = aktywnaKarta;
+        setTimeout(function(){
+            if (aktywne2Karty[0].className === aktywne2Karty[1].className){
+                aktywne2Karty.forEach( karta => {
+                    karta.classList.add ("off");
+                })
+            }
+        },500)
+    }
+}
 
 const poczatekGry = function() {
     karty.forEach(karta => {
@@ -20,6 +44,7 @@ const poczatekGry = function() {
     setTimeout(function(){
         karty.forEach(karta => {
             karta.classList.add("hidden");
+            karta.addEventListener('click', klikniecieKarty);
         })
     }, 600);
     }
